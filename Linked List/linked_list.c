e787b4b96aef5788817426d1230922353f706731
goofyNugtz <rahulranjan25.RR@gmail.com>
2021-08-26 00:54:03 +0530

    Added insert_before() method

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* 
 Roll No: 20CS8016
-Q: Implementation of a Linked List.
+Q: Write an algorithm for inserting an element in a singly linked linear list before a node whose value is val. 
+  Consider different possible scenarios for insertion: 
+  (i) at the  beginning, 
+  (ii) in the middle, 
+  (iii) at the end or
+  (iv) may not be inserted.
 */
 
 struct node {
@@ -14,6 +20,7 @@ struct node {
 struct node * head = NULL;
 
 
+//
 void insert_at_top(int value) {
   
   struct node *newptr = (struct node*)malloc(sizeof(struct node));
@@ -44,6 +51,44 @@ void push(int value) {
 
     temp -> next_node = newptr;
     newptr -> next_node = NULL;
+  }
+}
+
+
+void insert_before(int number, int val) {
+
+  struct node *newptr = (struct node *)malloc(sizeof(struct node));
+
+  newptr -> data = number;
+
+  if (head == NULL){
+    insert_at_top(number);
+
+  } else {
+    struct node * current_node, * previous_node;
+    current_node = head;
+    previous_node = NULL;
+
+    bool found = 0;
+
+    while (current_node -> next_node != NULL){
+      
+      if (current_node -> data != val){
+        previous_node = current_node;
+        current_node = current_node -> next_node;
+
+      } else if (current_node -> data == val){
+        previous_node -> next_node = newptr;
+        newptr -> next_node = current_node;
+
+        found = 1;
+        break;
+      }
+    }
+
+    if (!found) {
+      push(number);
+    }
   }
 }
 
@@ -83,6 +128,8 @@ void display_list() {
   }
 }
 
+
+
 int main () {
 
   create_list();
@@ -90,6 +137,8 @@ int main () {
   push(6);
   push(3);
   push(8);
+
+  insert_before(69, 10);
   display_list();
 
   return 0;
